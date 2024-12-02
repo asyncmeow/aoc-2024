2ff6dc40c2a14f969fbfe5421e1722d4895d70ca
pearl <pearl@rrr.sh>
2024-12-01 22:20:30 -0500

    day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,40 @@
 ﻿namespace AdventOfCode;
 
-public class Day01 : BaseDay
+public sealed class Day01 : BaseDay
 {
-    private readonly string _input;
+    public int[] Left { get; set; }
+    public int[] Right { get; set; }
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        var inTuples = File.ReadAllLines(InputFilePath)
+            .Select(l => l.Split("   "))
+            .Select(l => (int.Parse(l[0]), int.Parse(l[1])))
+            .ToList();
+        Left = inTuples.Select(t => t.Item1).Order().ToArray();
+        Right = inTuples.Select(t => t.Item2).Order().ToArray();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var total = 0;
+        for (var i = 0; i < Left.Length; i++)
+        {
+            total += Math.Abs(Left[i] - Right[i]);
+        }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        return new ValueTask<string>(total.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var total = 0;
+        foreach (var item in Left)
+        {
+            var count = Right.Count(i => i == item);
+            total += item * count;
+        }
+
+        return new ValueTask<string>(total.ToString());
+    }
 }
